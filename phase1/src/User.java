1d0203b87ff1b1bc727b71312b1a5db826e4332f
SarahAlk <sarah.aliakbari@mail.utoronto.ca>
2020-06-19 15:57:54 -0400

    Updated User entity class.

--- phase1/src/User.java
+++ phase1/src/User.java
@@ -1,4 +1,16 @@
 class User {
+    private String userName;
+    private String pw;
+    private String email;
+    private boolean isAdmin;
+
+    public User(String userName, String pw, String email){
+        this.userName = userName;
+        this.pw = pw;
+        this.email = pw;
+        isAdmin = false;
+    }
+
     if (true){
 
     }
