31ec00205c6609324de299de7da59b6670c1ced4
zhuwarre <warren.zhu@mail.utoronto.ca>
2020-06-19 23:28:59 -0400

    Added the id to item

--- phase1/src/Item.java
+++ phase1/src/Item.java
@@ -1,7 +1,9 @@
 abstract class Item{
 
     private String name;
-    private String type; //TODO: determine what types of items we have. (e.g. clothes, books,...)
+    private String type;
+    private Integer id;
+    //TODO: determine what types of items we have. (e.g. clothes, books,...)
 
     public Item(String name, String type){
         this.name = name;
