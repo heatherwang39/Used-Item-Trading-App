eb18b69573be6d84103bfe588cbd30d3d296cc4e
zhuwarre <warren.zhu@mail.utoronto.ca>
2020-06-28 00:45:51 -0400

    Tidied up Trade Manager

--- phase1/src/main/java/TradeManager.java
+++ phase1/src/main/java/TradeManager.java
@@ -6,41 +6,55 @@ public class TradeManager {
     private int numTrades = 0;
     private List<Trade> trades;
 
-    //TODO: Create a public method that creates new trades of the required type, using the helper methods below
+    //TODO: Create a public method that creates TwoPersonMeetings and moves it into a Trade
 
-    private void newOWPTrade(UserAccount sender, UserAccount receiver, List<Item> items){
-        Trade t = new OneWayPermanentTrade(numTrades + 1, sender, receiver, items);
+
+    //TODO: Write the JavaDoc for this Method
+    public void newOneWayTrade(Boolean permanent, UserAccount sender, UserAccount receiver, Item item){
+        if(permanent){newOWPTrade(sender, receiver, item);}
+        else{newOWTTrade(sender, receiver, item);}
+    }
+
+    private void newOWPTrade(UserAccount sender, UserAccount receiver, Item item){
+        Trade t = new OneWayPermanentTrade(numTrades + 1, sender, receiver, item);
         numTrades++;
         trades.add(t);
     }
 
-    private void newOWTTrade(UserAccount sender, UserAccount receiver, List<Item> items){
-        Trade t = new OneWayTemporaryTrade(numTrades + 1, sender, receiver, items);
-        numTrades++;
-        trades.add(t);
-    }
-
-    private void newTWPTrade(UserAccount trader1, List<Item> items1, UserAccount trader2, List<Item> items2){
-        Trade t = new TwoWayPermanentTrade(numTrades + 1, trader1, items1, trader2, items2);
-        numTrades++;
-        trades.add(t);
-    }
-
-    private void newTWTTrade(UserAccount trader1, List<Item> items1, UserAccount trader2, List<Item> items2){
-        Trade t = new TwoWayTemporaryTrade(numTrades + 1, trader1, items1, trader2, items2);
+    private void newOWTTrade(UserAccount sender, UserAccount receiver, Item item){
+        Trade t = new OneWayTemporaryTrade(numTrades + 1, sender, receiver, item);
         numTrades++;
         trades.add(t);
     }
 
 
+    //TODO: Write the JavaDoc for this Method
+    public void newTwoWayTrade(Boolean permanent, UserAccount firstTrader, Item firstItem,
+                               UserAccount secondTrader, Item secondItem){
+        if(permanent){newTWPTrade(firstTrader, firstItem, secondTrader, secondItem);}
+        else{newTWTTrade(firstTrader, firstItem, secondTrader, secondItem);}
+    }
 
-    public Trade getTrade(int tradeNumber){
+    private void newTWPTrade(UserAccount firstTrader, Item firstItem, UserAccount secondTrader, Item secondItem){
+        Trade t = new TwoWayPermanentTrade(numTrades + 1, firstTrader, firstItem, secondTrader, secondItem);
+        numTrades++;
+        trades.add(t);
+    }
+
+    private void newTWTTrade(UserAccount firstTrader, Item firstItem, UserAccount secondTrader, Item secondItem){
+        Trade t = new TwoWayTemporaryTrade(numTrades + 1, firstTrader, firstItem, secondTrader, secondItem);
+        numTrades++;
+        trades.add(t);
+    }
+
+
+    //TODO:Write the JavaDoc for this Method
+    public Trade getTrade(int tradeNumber) throws TradeNumberException{
         for(Trade t:trades){
             if(t.getTradeNumber() == tradeNumber){
                 return t;
             }
         }
-        //TODO: Create an Error if no main.java.Trade has the given TradeNumber
+        throw new TradeNumberException();
     }
-
 }
