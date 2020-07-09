45488e127664a8565d4c1e01d3271f3528711601
Charles Wang <charles.wang543@gmail.com>
2020-07-09 14:48:07 -0400

    Finished the outline for the text UI. Began implementing some required features.

--- phase1/src/main/java/TraderClient.java
+++ phase1/src/main/java/TraderClient.java
@@ -20,19 +20,24 @@ public class TraderClient {
 
 
     public void run() throws IOException, ClassNotFoundException {
-        System.out.println("Welcome to Phase1 Project. At anytime you may type 'exit' to quit.");
+        System.out.println("Welcome to Trader. At anytime you may type 'exit' to quit.\n" +
+                "Please choose any of the following by typing the option number.");
         Account currUser;
-        System.out.println("Please choose any of the following by typing the option number.");
+
         try (BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in))) { //from readWrite lecture
             TraderSystem as = new TraderSystem(accountsPath);
-            System.out.println("1. Sign In    2.Register");
+            System.out.println("1. Sign In\n2.Register");
             String input = keyboard.readLine();
             currUser = as.login(input);
             System.out.println("Login Successful. Welcome to Trader, " + currUser.getUsername());
-            System.out.println("1. View Account information    2. View trade listings    3. View my trade activity");
-            if (currUser.isAdmin()){
-                System.out.println("4. Show messages    5. Edit Customer Account information");
+
+            if (curr.user.isUser()){
+                layerTwoUser(currUser);
             }
+            if (currUser.isAdmin()){
+                layerTwoAdmin();
+            }
+
         } catch (InvalidOptionException e) {
             System.out.println("Invalid Option detected. Please try again.");
         } catch (IOException e) {
@@ -44,7 +49,68 @@ public class TraderClient {
 
         //TODO: give options to view available/active/requested trade lists or user account info etc. UPDATE: create another presenter class
 
+    }
+
+
+    public void layerTwoUser(Account user) throws IOException {
+        try (BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.println("1. View Account Information\n2. Add Items\n3. Browse Listings\n4. My Activity\n" +
+                    "5. Offers\n6. Active Trades");
+            if (keyboard.readLine() == "1") {
+                System.out.println("Username: " + user.getUserName() + "\nEmail" + user.getEmail() + "\nInventory: " +
+                        user.getInventory() + "\nWishlist" + user.getWishlist());
+            } else if (keyboard.readLine() == "2") {
+                addItems();
+            } else if (keyboard.readLine() == "3") {
+                browseListings();
+            } else if (keyboard.readLine() == "4") {
+                ;
+            } else if (keyboard.readLine() == "5") {
+                ;
+            } else if (keyboard.readLine() == "6"){
+                ;
+            }
+
+
+        } catch (InvalidOptionException e) { // I have a suspicion that these try catches may be redundant, lmk if theres a way around it - Charles
+            System.out.println("Invalid Option detected. Please try again.");
+        } catch (IOException e) {
+            System.out.println("Something went wrong");
+
         }
+    }
+
+    public void layerTwoAdmin(){
+        try (BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.println("1. View Requests\n2. Freeze Accounts\n3. Update Trade Threshold\n4. Add Admins");
+
+            if (keyboard.readLine() == "1") {
+                // print an arraylist of all user requests of items to be added to their inventory,either accept or deny
+                ;
+            } else if (keyboard.readLine() == "2") {
+                // get and print out users the system thinks the admin should freeze
+                // this should take the input for each individual user, and accept either a yes or no
+                ;
+            } else if (keyboard.readLine() == "3") {
+                System.out.println("Enter the new trading threshold: ");
+            } else if (keyboard.readLine() == "4") {
+                // add new admins
+                ;
+            }
+        } catch (InvalidOptionException e) {
+            System.out.println("Invalid Option detected. Please try again.");
+        } catch (IOException e) {
+            System.out.println("Something went wrong");
+        }
+    }
+
+    public void addItems(){
+        ;
+    }
+
+    public void browseListings(){
+        ;
+    }
 
 
 
