3eca9ec9e93bc6104b3261e68293296fec686afb
heatherwang <heatherwang39@gmail.com>
2020-07-14 01:38:42 -0400

    Add javadocs for ItemNotFoundException and MessageNotFoundException

--- phase1/src/main/java/MessageNotFoundException.java
+++ phase1/src/main/java/MessageNotFoundException.java
@@ -1,7 +1,10 @@
 package main.java;
 
 /**
- * Exception to be thrown when an item cannot be found with the given itemId.
+ * Exception to be thrown when an account has no message.
+ * @author Heather Wang
+ * @version %I%, %G%
+ * @since Phase 1
  */
 public class MessageNotFoundException extends Exception{
 
